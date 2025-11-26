ee5e22e51fbb844cb68ec3a13dcffcfe0040e42b
amoseses <131488113+amoseses@users.noreply.github.com>
2025-11-25 23:03:54 -0500

    Delete extract_text.py

--- extract_text.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pdfminer.high_level import extract_text
-
-def extract_from_pdf(path):
-    return extract_text(path)
-
-if __name__ == '__main__':
-    p = sys.argv[1]
-    print(extract_from_pdf(p))
