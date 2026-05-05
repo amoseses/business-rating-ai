6ec7dee3faf69a4421b304bac0fb6f4f176ef168
amoseses <131488113+amoseses@users.noreply.github.com>
2026-05-05 11:30:21 -0400

    Delete astrapitch.config.example.js

--- astrapitch.config.example.js
+++
@@ -1,6 +0,0 @@
-window.ASTRAPITCH_CONFIG = {
-  // Copy this file to `astrapitch.config.js` and replace with your real Stripe Payment Link.
-  // Example: https://buy.stripe.com/abc123
-  // Tip: configure Stripe success_url to come back to this app with `?checkout=success`.
-  stripePaymentLink: 'https://buy.stripe.com/test_28EaEX8EP0gZcP44WX5os00'
-};
